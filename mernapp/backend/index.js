17bdfdebdf3ba22d85ba8a0595967006af46ba36
Shraddha Suman Mishra <sumanshraddha.mishra@gmail.com>
2024-04-28 21:43:17 +0530

    Sign Up and Login

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 4000;
 const mongoDB = require("./db");
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+})
 mongoDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.use(express.json());
+app.use('/api',require("./Routes/CreateUser"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
